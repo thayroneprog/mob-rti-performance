618e182c02579933b820d872eff3a7c2209756df
thayroneprog <thayronedev@gmail.com>
2024-01-29 02:07:15 -0300

    fix: adjust dockerfile

--- src/main/java/com/mob/infrasctructure/SwaggerConfig.java
+++ src/main/java/com/mob/infrasctructure/SwaggerConfig.java
@@ -1,32 +1,27 @@
 package com.mob.infrasctructure;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("seu.pacote.controllers"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("API Documentation")
-                .description("Descrição da sua API")
-                .version("1.0.0")
-                .build();
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("RTI Performance API")
+                                .description("API responsible for managing patients.")
+                                .version("v1")
+                                .license(
+                                        new License()
+                                                .name("Proprietary License")
+                                                .url(""))
+                                .contact(new Contact().name("Thayrone").email("thayrone.dev@gmail.com")));
     }
 }
